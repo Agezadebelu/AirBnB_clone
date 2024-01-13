528463ad3a2ed99f1a18e11ccc0a6ac21555af1b
Agezadebelu <lateradeb2019@gmail.com>
2024-01-13 13:51:30 -0800

    updated

--- models/__init__.py
+++ models/__init__.py
@@ -3,7 +3,7 @@
 Module: __init__.py
 """
 
-from models.engine.file_storage import FileStorage
+from models.engine import file_storage
 
 storage = file_storage.FileStorage()
 storage.reload()
--- models/base_model.py
+++ models/base_model.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """This script is the base model"""
 
+import models
 import uuid
 from datetime import datetime
 
@@ -27,7 +28,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            storage.new(self)
+            models.storage.new(self)
 
     def __str__(self):
         """Returns official string representation"""
@@ -39,11 +40,7 @@ class BaseModel:
         """updates the public instance attribute updated_at"""
 
         self.updated_at = datetime.now()
-        storage.save()
-
-    def some_function():
-        from models import storage
-        # rest of the code
+        models.storage.save()
 
     def to_dict(self):
         """returns a dictionary containing all keys/values of __dict__"""
