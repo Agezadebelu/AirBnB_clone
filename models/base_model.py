69732f24af4bd5526c66dcc4045851c5f6b9ceac
Agezadebelu <lateradeb2019@gmail.com>
2024-01-13 11:15:56 -0800

    updated

--- models/base_model.py
+++ models/base_model.py
@@ -10,7 +10,7 @@ class BaseModel:
     """Class from which all other classes will inherit"""
 
     def __init__(self, *args, **kwargs):
-                """Initializes instance attributes
+        """Initializes instance attributes
 
         Args:
             - *args: list of arguments
@@ -31,19 +31,19 @@ class BaseModel:
             models.storage.new(self)
 
     def __str__(self):
-         """Returns official string representation"""
+        """Returns official string representation"""
 
         return "[{}] ({}) {}".format(self.__class__.__name__,
                                      self.id, self.__dict__)
 
     def save(self):
-         """updates the public instance attribute updated_at"""
+        """updates the public instance attribute updated_at"""
 
         self.updated_at = datetime.now()
         models.storage.save()
 
     def to_dict(self):
-         """returns a dictionary containing all keys/values of __dict__"""
+        """returns a dictionary containing all keys/values of __dict__"""
 
         obj_dict = self.__dict__.copy()
         obj_dict['__class__'] = self.__class__.__name__
