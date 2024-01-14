f203fe73a18a24824fcddc1215bbd655b1a707ef
Agezadebelu <lateradeb2019@gmail.com>
2024-01-13 21:45:20 -0800

    updated

--- models/base_model.py
+++ models/base_model.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """This script is the base model"""
 
-import models
+from models import storage
 import uuid
 from datetime import datetime
 
@@ -28,7 +28,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            models.storage.new(self)
+            storage.new(self)
 
     def __str__(self):
         """Returns official string representation"""
@@ -40,7 +40,7 @@ class BaseModel:
         """updates the public instance attribute updated_at"""
 
         self.updated_at = datetime.now()
-        models.storage.save()
+        storage.save()
 
     def to_dict(self):
         """returns a dictionary containing all keys/values of __dict__"""
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,13 +1,6 @@
 import json
 import datetime
-from os.path import isfile
-from models.base_model import BaseModel
-from models.user import User
-from models.state import State
-from models.city import City
-from models.review import Review
-from models.amenity import Amenity
-from models.place import Place
+import os
 
 
 class FileStorage:
@@ -29,18 +22,35 @@ class FileStorage:
         with open(FileStorage.__file_path, 'w') as file:
             json.dump(serialized_objects, file)
 
+    def classes(self):
+        """Returns a dictionary of valid classes and their references"""
+        from models.base_model import BaseModel
+        from models.user import User
+        from models.state import State
+        from models.city import City
+        from models.amenity import Amenity
+        from models.place import Place
+        from models.review import Review
+
+        classes = {"BaseModel": BaseModel,
+                   "User": User,
+                   "State": State,
+                   "City": City,
+                   "Amenity": Amenity,
+                   "Place": Place,
+                   "Review": Review}
+        return classes
+
     def reload(self):
-        if isfile(FileStorage.__file_path):
-            with open(FileStorage.__file_path, 'r') as file:
-                try:
-                    loaded_objects = json.load(file)
-                    for key, value in loaded_objects.items():
-                        class_name, instance_id = key.split('.')
-                        class_obj = globals()[class_name]
-                        instance = class_obj(**value)
-                        FileStorage.__objects[key] = instance
-                except json.JSONDecodeError:
-                    pass
+        """Reloads the stored objects"""
+        if not os.path.isfile(FileStorage.__file_path):
+            return
+        with open(FileStorage.__file_path, "r", encoding="utf-8") as f:
+            obj_dict = json.load(f)
+            obj_dict = {k: self.classes()[v["__class__"]](**v)
+                        for k, v in obj_dict.items()}
+            # TODO: should this overwrite or insert?
+            FileStorage.__objects = obj_dict
 
     def attributes(self):
         """Returns the valid attributes and their types for classname"""
