a6c90a9967152830309bdb25b19a88d6045f1897
Agezadebelu <lateradeb2019@gmail.com>
2024-01-14 00:56:50 -0800

    updated

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,20 +1,28 @@
+#!/usr/bin/python3
+"""Module for FileStorage class."""
+
 import json
 import datetime
 import os
 
 
 class FileStorage:
+     """Class for storing and retrieving data"""
+
     __file_path = "file.json"
     __objects = {}
 
     def all(self):
+        """returns the dictionary __objects"""
         return FileStorage.__objects
 
     def new(self, obj):
+        """sets in __objects the obj with key <obj class name>.id"""
         key = "{}.{}".format(obj.__class__.__name__, obj.id)
         FileStorage.__objects[key] = obj
 
     def save(self):
+        """ serializes __objects to the JSON file (path: __file_path)"""
         serialized_objects = {}
         for key, value in FileStorage.__objects.items():
             serialized_objects[key] = value.to_dict()
